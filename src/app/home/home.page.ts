4add7e4cf9cf78530a7321d598f4d19d2a25fdf6
kdss-mila <mila.kdss@gmail.com>
2019-01-31 04:55:43 -0400

    grafico 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import chartJS from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('barCanvas') barCanvas;
+  //@ViewChild('pieCanvas') pieCanvas;
 
+
+  barChart: any;
+  //pieChart: any;
+
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.barChart = this.getBarChart();
+      //this.pieChart = this.getPieChart();
+    }, )
+  }
+
+   getChart(context, charType, data, options) {
+    return new chartJS(context,{
+      data,
+      options,
+      type: charType
+    })
+  }
+
+  getBarChart() {
+    const data = {
+      labels: ['vermelho', 'Azul', 'Amarelo', 'verde', 'roxo'],
+      datasets: [{
+        label: 'numero',
+        data: [12, 23, 15, 90, 5],
+        backgroundColor: [
+          'rgb(255,0,0)',
+          'rgb(20,0,255)',
+          'rgb(0,255,10)',
+          'rgb(255,230,0)',
+          'rgb(60,0,70)',
+
+        ],
+        borderWidth: 1
+      }]
+    };
+
+    const options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginZero: true
+          }
+        }]
+      }
+    }
+    return this.getChart(this.barCanvas.nativeElement, 'bar', data, options);
+
+  }
 }
